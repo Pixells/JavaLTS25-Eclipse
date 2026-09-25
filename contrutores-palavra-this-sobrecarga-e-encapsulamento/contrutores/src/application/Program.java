d783c770af13a6844c8e84d34e17c006e91e9df9
Erik Matheus <80124753+Pixells@users.noreply.github.com>
2026-09-25 16:28:05 -0300

    Encapsulamento
    Aula e testes da classe com private

--- contrutores-palavra-this-sobrecarga-e-encapsulamento/contrutores/src/application/Program.java
+++ contrutores-palavra-this-sobrecarga-e-encapsulamento/contrutores/src/application/Program.java
@@ -21,6 +21,13 @@ public class Program {
 		Product product = new Product(name, price);
 		
 		System.out.println();
+		product.setName("Computador");
+		System.out.println("Updated name: " + product.getName());
+		
+		product.setPrice(1200.00);
+		System.out.println("Updated price: " + product.getPrice());
+		
+		System.out.println();
 		System.out.println("Product data: " + product);
 		
 		System.out.println();
--- contrutores-palavra-this-sobrecarga-e-encapsulamento/contrutores/src/entities/Product.java
+++ contrutores-palavra-this-sobrecarga-e-encapsulamento/contrutores/src/entities/Product.java
@@ -2,9 +2,12 @@ package entities;
 
 public class Product {
 
-	public String name;
-	public double price;
-	public int quantity;
+	private String name;
+	private double price;
+	private int quantity;
+	
+	public Product() {
+	}
 	
 	public Product(String name, double price, int quantity) {
 		this.name = name;
@@ -14,6 +17,26 @@ public class Product {
 	
 	public Product(String name, double price) {
 		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+		
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPrice(double price) {
 		this.price = price;
 	}
 	
